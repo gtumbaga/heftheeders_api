ffaecf504f8da8f05f544290b153e226e19f2ccc
Ubuntu <ubuntu@ip-172-31-11-194.us-west-2.compute.internal>
2024-03-03 14:43:24 -0800

    added item model

---
+++ app/Models/Item.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'serving_unit',
+        'serving_size',
+        'serving_quantity',
+        'calories',
+    ];
+}
